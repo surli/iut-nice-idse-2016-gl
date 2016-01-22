eefdaa6b88d6e25f37aafb44077030ea0458223d
Baptiste Etienne <baptiste.etienne@etu.unice.fr>
2016-01-22 12:00:02 +0100

    LPSUA-5 : Réalisation constructeurs

---
+++ src/main/model/Board.java
@@ -0,0 +1,13 @@
+import java.util.ArrayList;
+
+public class Board {
+	private ArrayList<Player> players;
+	public Board()
+	{
+		this.players = new ArrayList<Player>();
+	}
+	public Board(ArrayList<Player> players)
+	{
+		this.players = players;
+	}
+}
