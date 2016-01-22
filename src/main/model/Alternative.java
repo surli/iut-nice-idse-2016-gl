40cb1f537eda6e14fef7760626988aae5ace4d95
Baptiste Etienne <baptiste.etienne@etu.unice.fr>
2016-01-22 12:04:38 +0100

    LPSUA-11 : Constructeurs

---
+++ src/main/model/Alternative.java
@@ -0,0 +1,16 @@
+import java.util.ArrayList;
+import java.util.HashMap;
+
+public class Alternative {
+	private HashMap<String,ArrayList<Runnable>> actions;
+	
+	public Alternative()
+	{
+		actions = new HashMap<String, ArrayList<Runnable>>();
+	}
+	
+	public void addActionToCard(String card,Runnable action)
+	{
+		
+	}
+}
