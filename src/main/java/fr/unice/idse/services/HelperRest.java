c37243de8f5cea9aa16550e60f05021d4f21eff8
Jeremie Elbaz <jeremie-e@hotmail.fr>
2016-01-25 11:27:25 +0100

    LPSUA-20 : Ajout d'une méthode listant toutes les parties du jeu

---
+++ src/main/java/fr/unice/idse/services/HelperRest.java
@@ -0,0 +1,27 @@
+package fr.unice.idse.services;
+
+import fr.unice.idse.model.Game;
+import fr.unice.idse.model.Model;
+
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.Response;
+import java.util.HashMap;
+
+@Path("/helper")
+public class HelperRest {
+
+    @GET
+    @Path("games")
+    @Produces(MediaType.APPLICATION_JSON)
+    public Response listGame(){
+        Model model = Model.getInstance();
+        HashMap<String, String> list = new HashMap<>();
+        for (Game game : model.getGames()){
+            list.put(game.getName(), )
+        }
+        return Response.status(200).entity("").build();
+    }
+}
