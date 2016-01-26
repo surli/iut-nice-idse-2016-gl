f8560854cfb8ea2d114f4a989d22607e9a9d8ad0
Tom Phily <tom.phily@live.fr>
2016-01-26 15:22:14 +0100

    LPSUA-5 : Ajout daccesseur a la liste de joueur dans board

--- src/main/java/fr/unice/idse/model/Board.java
+++ src/main/java/fr/unice/idse/model/Board.java
@@ -2,14 +2,37 @@ package fr.unice.idse.model;
 
 import java.util.ArrayList;
 
-public class Board {
+public class Board 
+{
+	
 	private ArrayList<Player> players;
+	private Alternative variante;
+	private int actualPlayer =0;
+	
 	public Board()
 	{
 		this.players = new ArrayList<Player>();
+		variante = new Alternative();
 	}
-	public Board(ArrayList<Player> players)
+	
+	/*public Board(ArrayList<Player> players)
 	{
 		this.players = players;
+		this.variante = new Alternative();
+	}*/	
+	
+	public ArrayList<Player> getPlayers()
+	{
+		return players;
+	}
+	
+	public void setPlayers(ArrayList<Player> players)
+	{
+		this.players = players;
+	}
+	
+	public Player getActualPlayer()
+	{
+		return players.get(actualPlayer);
 	}
 }
