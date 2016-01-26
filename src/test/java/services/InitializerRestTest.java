32131667bc14e0ccdf075ee581927849e1945970
Jocelin Heinen <johei@ymail.com>
2016-01-26 15:11:57 +0100

    LPSUA-13 doc InitializerRestTest

--- src/test/java/services/InitializerRestTest.java
+++ src/test/java/services/InitializerRestTest.java
@@ -13,26 +13,34 @@ import fr.unice.idse.constante.Config;
 
 public class InitializerRestTest {
 
-		
-//		@Test
-//		public void creategameTest(){
-//			given().when().post("/creategame").then().statusCode(200);
-//		}
+		/**
+		 * Test du lancement de la partie en Rest
+		 * On passe comme argument un token, un nom de partie, un nom de joueur
+		 * On test que si tout les parametres sont bon on a un code retour de 200
+		 */
 	
 		@Test
 		public void createTest(){
 	
+		/**
+		 * Creation dun tableau formaté JSON avec les 3 parametres	
+		 */
+			
 		Map<String, Object>  jsonAsMap = new HashMap<>();
 		jsonAsMap.put("_token", "hbj7BB7Y6B87T282B87T27N90A098");
 		jsonAsMap.put("game", "Superfly");
 		jsonAsMap.put("playername", "marcel");
-
+		
+	
 			
 		given().
 		contentType(com.jayway.restassured.http.ContentType.JSON).
 		body(jsonAsMap).
         when(). 
         post("http://localhost:8080/rest/initializer/creategame").
+    	/**
+		 * on verifie que le code de retour est bien 200/succes	
+		 */
         then().statusCode(200);
 		}
 		
