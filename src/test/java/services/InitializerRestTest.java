3f8d5622ba599d8b50317801a08eb018c2eabc05
Jeremie Elbaz <jeremie-e@hotmail.fr>
2016-01-29 17:39:22 +0100

    LPSUA-13 : retranscription du test

--- src/test/java/services/InitializerRestTest.java
+++ src/test/java/services/InitializerRestTest.java
@@ -1,53 +1,48 @@
 package services;
 
-import static com.jayway.restassured.RestAssured.expect;
 import static com.jayway.restassured.RestAssured.given;
+import static org.junit.Assert.assertEquals;
 
 import java.util.HashMap;
 import java.util.Map;
 
-import org.apache.http.entity.ContentType;
+import fr.unice.idse.services.InitializerRest;
+import org.glassfish.jersey.server.ResourceConfig;
+import org.glassfish.jersey.test.JerseyTest;
 import org.junit.Test;
 
-import fr.unice.idse.constante.Config;
+import javax.ws.rs.client.Entity;
+import javax.ws.rs.core.Application;
+import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.Response;
 
-public class InitializerRestTest {
+public class InitializerRestTest extends JerseyTest {
 
+
+		@Override
+		protected Application configure() {
+			return new ResourceConfig(InitializerRest.class);
+		}
 		/**
 		 * Test du lancement de la partie en Rest
 		 * On passe comme argument un token, un nom de partie, un nom de joueur
 		 * On test que si tout les parametres sont bon on a un code retour de 200
 		 */
 	
-		/*@Test
+		@Test
 		public void createTest(){
-	
-		/**
-		 * Creation dun tableau formaté JSON avec les 3 parametres	
-		 */
-			
-		/*Map<String, Object>  jsonAsMap = new HashMap<String, Object>();
-		jsonAsMap.put("_token", "hbj7BB7Y6B87T282B87T27N90A098");
-		jsonAsMap.put("game", "Superfly");
-		jsonAsMap.put("playername", "marcel");
-		
-	
-			
-		given().
-		contentType(com.jayway.restassured.http.ContentType.JSON).
-		body(jsonAsMap).
-        when(). 
-        post("http://localhost:8080/rest/initializer/creategame").
-    	/**
-		 * on verifie que le code de retour est bien 200/succes	
-		 */
-        /*then().statusCode(200);
-		}
-		
+			/**
+			 * Creation dun tableau formaté JSON avec les 3 parametres
+			 */
+			String json = "{_token: 'hbj7BB7Y6B87T282B87T27N90A098', game: 'superfly', player: 'marcel'}";
+			Entity<String> jsonEntity = Entity.entity(json, MediaType.APPLICATION_JSON);
 
-	//@Test
-	//verifier authentification si besoin
-	//given().auth().basic(username, password).when().get("/secured").then().statusCode(200);*/
+			/**
+			 * on verifie que le code de retour est bien 200/succes
+			 */
+			Response response = target("/initializer/creategame").request().post(jsonEntity);
+			assertEquals(200, response.getStatus());
+		}
 	}
 
 
