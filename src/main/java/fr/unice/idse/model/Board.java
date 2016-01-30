e517f0bc9c44035b901e033c566fd8acae2957bb
Tom Phily <tom.phily@live.fr>
2016-01-30 18:46:41 +0100

    LPSUA-5 test initialisation du plateau de jeu

--- src/main/java/fr/unice/idse/model/Board.java
+++ src/main/java/fr/unice/idse/model/Board.java
@@ -7,14 +7,18 @@ public class Board
 	
 	private ArrayList<Player> players;
 	private Alternative variante;
-	private int actualPlayer = 0;
+	private int actualPlayer;
 	private Stack stack;
 	private Deck deck;
+	private Color actualColor;
 	
 	public Board()
 	{
 		this.players = new ArrayList<Player>();
 		variante = new Alternative();
+		deck = new Deck();
+		stack = new Stack();
+		actualPlayer = 0;
 	}
 	
 	/*public Board(ArrayList<Player> players)
@@ -47,22 +51,33 @@ public class Board
 	}
 	
 	/**
-	 * Change la couleur de la carte supérieur du talon
+	 * Change la couleur actuel du jeu
 	 * @param color
 	 */
+	//Je suis pas trop daccord sur le fait de changer la couleur de la carte 
+	//car celà impliquerait qu'il y ai une carte d'une couleur final en plus 
+	//et une carte de couleur final en moins si vous voyez ce que je veu dire
 	public void changeColor(Color color)
 	{
-		getStack().changeColor(color);
+		actualColor = color;
 	}
 	
 	/**
-	 * Initialise le plateau
+	 * Initialise le plateau de jeu
 	 */
 	public void init()
 	{
+		deck.fillDeck();
+		deck.shuffle();
 		for(int i = 0; i<players.size(); i++)
 		{
-			players.get(i).getCards().add(null);
+			for(int j = 0; j < 7; j++)
+			{
+				players.get(i).getCards().add(deck.topCard());
+				deck.removeTopCard();
+			}
 		}
+		stack.initStack(deck);
+		actualColor = stack.topCard().getColor();
 	}
 }
--- src/test/java/model/BoardTest.java
+++ src/test/java/model/BoardTest.java
@@ -4,11 +4,23 @@ import static org.junit.Assert.*;
 
 import org.junit.Test;
 
-public class BoardTest {
+import fr.unice.idse.model.Board;
+import fr.unice.idse.model.Player;
+
+public class BoardTest 
+{
 
 	@Test
-	public void test() {
-		fail("Not yet implemented");
+	public void testInitBoard() 
+	{
+		Board board = new Board();
+		board.getPlayers().add(new Player("toto"));
+		board.getPlayers().add(new Player("test"));
+		board.init();
+		assertFalse(board.getDeck().isEmpty());
+		assertTrue(board.getPlayers().get(0).getCards().size()>0);
+		assertTrue(board.getPlayers().get(1).getCards().size()>0);
+		assertTrue(board.getStack().getStack().size()>0);
 	}
 
 }
