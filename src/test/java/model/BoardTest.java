adc1d1d15cce33e0bff182af39b3893b17cc2ba4
Tom Phily <tom.phily@live.fr>
2016-01-31 04:41:36 +0100

    LPSUA-5 : ajout de test pour le tour dun joueur ainsi que different control pour savoir si une carte est posable

--- src/test/java/model/BoardTest.java
+++ src/test/java/model/BoardTest.java
@@ -5,6 +5,8 @@ import static org.junit.Assert.*;
 import org.junit.Test;
 
 import fr.unice.idse.model.Board;
+import fr.unice.idse.model.Card;
+import fr.unice.idse.model.Color;
 import fr.unice.idse.model.Player;
 
 public class BoardTest 
@@ -23,4 +25,50 @@ public class BoardTest
 		assertTrue(board.getStack().getStack().size()>0);
 	}
 
+	@Test
+	public void testCarteBonneValeurEstJouable()
+	{
+		Board board = new Board();
+		board.getStack().addCard(new Card(8, Color.Noir));
+		board.changeColor(board.getStack().topCard().getColor());
+		assertTrue(board.AskPlayableCard(new Card(8, Color.Rouge)));
+	}
+	
+	@Test
+	public void testCarteMauvaiseValeurEstNonJouable()
+	{
+		Board board = new Board();
+		board.getStack().addCard(new Card(8, Color.Bleu));
+		board.changeColor(board.getStack().topCard().getColor());
+		assertFalse(board.AskPlayableCard(new Card(7, Color.Jaune)));
+	}
+	
+	@Test
+	public void testCarteBonneCouleurEtMauvaiseValeurEstJouable()
+	{
+		Board board = new Board();
+		board.getStack().addCard(new Card(8, Color.Rouge));
+		board.changeColor(board.getStack().topCard().getColor());
+		assertTrue(board.AskPlayableCard(new Card(7, Color.Rouge)));
+	}
+	
+	@Test
+	public void testCarteMauvaiseCouleurEtMauvaiseValeurEstNonJouable()
+	{
+		Board board = new Board();
+		board.getStack().addCard(new Card(8, Color.Vert));
+		board.changeColor(board.getStack().topCard().getColor());
+		assertFalse(board.AskPlayableCard(new Card(7, Color.Rouge)));
+	}
+	
+	@Test
+	public void testJoueurPeutJouer()
+	{
+		Board board = new Board();
+		board.getPlayers().add(new Player("toto"));
+		board.getActualPlayer().getCards().add(new Card(7, Color.Rouge));
+		board.getStack().addCard(new Card(8, Color.Rouge));
+		board.changeColor(board.getStack().topCard().getColor());
+		assertTrue(board.AskPlayerCanPlay(board.getActualPlayer()));
+	}
 }
