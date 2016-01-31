333c868c548678c5bc2e2e9e85f9a60b67258e88
Jeremie Elbaz <jeremie-e@hotmail.fr>
2016-01-31 16:27:42 +0100

    LPSUA-33 : Creation de la route + Ajout de 2 tests + Modif test Lobby

--- src/main/java/fr/unice/idse/services/GameRest.java
+++ src/main/java/fr/unice/idse/services/GameRest.java
@@ -5,17 +5,23 @@ import fr.unice.idse.model.Model;
 
 import javax.ws.rs.*;
 import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.Response;
 
 @Path("/game")
 public class GameRest {
 
-    @POST
+
+    @GET
     @Path("{gamename}/gamestate")
     @Produces(MediaType.APPLICATION_JSON)
-    public void isStarted(@PathParam("gamename") String gamename, String objJSON){
+    public Response isStarted(@PathParam("gamename") String gamename, String objJSON){
         Model model = Model.getInstance();
         Game game = model.findGameByName(gamename);
 
+        if(game == null)
+            return Response.status(500).entity("Partie inconnu").build();
+
+        return Response.status(200).entity("{state: "+game.getBoard().gameBegin()+"}").build();
 
     }
 }
