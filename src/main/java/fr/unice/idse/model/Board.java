3f9abc7f8d1f05e0c964bcca7b749acdf6fcf2eb
Tom Phily <tom.phily@live.fr>
2016-01-31 03:49:37 +0100

    LPSUA-5 : ajout de methode pour le tour dun joueur ainsi que different control pour savoir si une carte est posable

--- src/main/java/fr/unice/idse/model/Board.java
+++ src/main/java/fr/unice/idse/model/Board.java
@@ -11,6 +11,8 @@ public class Board
 	private Stack stack;
 	private Deck deck;
 	private Color actualColor;
+	private boolean meaning;
+	private int maxPlayer;
 	
 	public Board()
 	{
@@ -19,6 +21,8 @@ public class Board
 		deck = new Deck();
 		stack = new Stack();
 		actualPlayer = 0;
+		meaning = true;
+		maxPlayer = 4;
 	}
 	
 	/*public Board(ArrayList<Player> players)
@@ -26,6 +30,23 @@ public class Board
 		this.players = players;
 		this.variante = new Alternative();
 	}*/	
+	
+	public void ChangeMeaning()
+	{
+		meaning = !meaning;
+	}
+	
+	public void NextPlayer()
+	{
+		if(meaning)
+		{
+			actualPlayer = (actualPlayer+1)%maxPlayer;
+		}
+		else
+		{
+			actualPlayer = (actualPlayer-1)%maxPlayer;
+		}
+	}
 	
 	public ArrayList<Player> getPlayers()
 	{
@@ -56,7 +77,7 @@ public class Board
 	 */
 	//Je suis pas trop daccord sur le fait de changer la couleur de la carte 
 	//car celà impliquerait qu'il y ai une carte d'une couleur final en plus 
-	//et une carte de couleur final en moins si vous voyez ce que je veu dire
+	//et une carte de couleur depart en moins si vous voyez ce que je veu dire
 	public void changeColor(Color color)
 	{
 		actualColor = color;
@@ -69,15 +90,76 @@ public class Board
 	{
 		deck.fillDeck();
 		deck.shuffle();
-		for(int i = 0; i<players.size(); i++)
+		for(Player player : players)
 		{
 			for(int j = 0; j < 7; j++)
 			{
-				players.get(i).getCards().add(deck.topCard());
+				player.getCards().add(deck.topCard());
 				deck.removeTopCard();
 			}
 		}
 		stack.initStack(deck);
 		actualColor = stack.topCard().getColor();
+	}
+	
+	/**
+	 * Retourne la liste des cartes jouables
+	 */
+	public ArrayList<Card> PlayableCards()
+	{
+		ArrayList<Card> playableCards = new ArrayList<Card>();
+		for(Card card : getActualPlayer().getCards())
+		{
+			if(AskPlayableCard(card))
+			{
+				playableCards.add(card);
+			}
+		}
+		return playableCards;
+	}
+	
+	/**
+	 * Retourne si une carte est jouable
+	 * @param Card
+	 */
+	public boolean AskPlayableCard(Card card)
+	{
+		return card.getValue() == stack.topCard().getValue() || card.getColor().equals(actualColor);
+	}
+	
+	/**
+	 * Retourne si le joueur peut jouer
+	 * @param player
+	 * @return
+	 */
+	public boolean AskPlayerCanPlay(Player player)
+	{
+		return player.equals(getActualPlayer()) && PlayableCards().size() > 0;
+	}
+	
+	/**
+	 * Joue le tour du joueur actuel en posant une carte dans la fosse.
+	 * @param card
+	 */
+	public void PoseCard(Card card)
+	{
+		//Control si la carte est posable
+		if(AskPlayableCard(card))
+		{
+			stack.addCard(card);
+			actualColor = card.getColor();
+			getActualPlayer().getCards().remove(card);
+			NextPlayer();
+		}
+	}
+	
+	/**
+	 * Le joueur actuel pioche une carte, ne fait pas passer le tour pour le cas
+	 * où il subit une carte à effet spécial.
+	 */
+	public void Pioche()
+	{
+		getActualPlayer().getCards().add(deck.topCard());
+		deck.removeTopCard();
 	}
 }
