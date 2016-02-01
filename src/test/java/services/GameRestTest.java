1b79cb9b3785b1df4afc4fa7933da2677d5a64a0
Jocelin Heinen <johei@ymail.com>
2016-02-01 20:25:25 +0100

    LPSUA-13 get hand player + test commented

--- src/test/java/services/GameRestTest.java
+++ src/test/java/services/GameRestTest.java
@@ -7,9 +7,14 @@ import fr.unice.idse.services.GameRest;
 import org.codehaus.jettison.json.*;
 import org.glassfish.jersey.server.ResourceConfig;
 import org.glassfish.jersey.test.JerseyTest;
+import org.json.JSONArray;
 import org.junit.Before;
 import org.junit.Test;
 
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
+import javax.ws.rs.Produces;
 import javax.ws.rs.client.Entity;
 import javax.ws.rs.core.Application;
 import javax.ws.rs.core.MediaType;
@@ -70,4 +75,30 @@ public class GameRestTest extends JerseyTest {
         assertEquals(405, response.getStatus());
     }
 
+//    @Test 
+//    public void handPlayerTest() throws JSONException{
+//        	 Player player = model.findPlayerByName("tata", "toto");
+//                    
+//            Color color = null;
+//            int value = 0;
+//            int i;
+//            int taille =  player.getCards().size();
+//            
+//    		JSONObject obj = new JSONObject();
+//     
+//    		JSONArray listofPLayercards = new JSONArray();
+//    		
+//    		
+//     		for (i = 0; i < taille; i++) {
+//     			color=player.getCards().get(i).getColor();
+//     			value=player.getCards().get(i).getValue();
+//     			listofPLayercards.put("\"number\": "+value+"");
+//     			listofPLayercards.put("\"familly\": "+color+"");
+//     			listofPLayercards.put("\"idcard\": "+i+"");
+//     			obj.put("carte", listofPLayercards);
+//     		}
+//     		
+//    	     Response.ok(obj, MediaType.APPLICATION_JSON).build();
+//        }  
+    
 }
