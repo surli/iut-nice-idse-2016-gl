ce2c727ccc56e11c7b0095bf7f9e191ae21091f8
jeremy <jeremyfroment@yahoo.fr>
2016-02-08 11:30:49 +0100

    LPSUA-79

--- webuno/app/scripts/controllers/RoomController.js
+++ webuno/app/scripts/controllers/RoomController.js
@@ -1,15 +1,29 @@
 'use strict';
 
 angular.module('unoApp')
-    .controller('RoomController', ['$rootScope', '$scope', '$state', '$stateParams', '$http', function ($rootScope, $scope, $state, $stateParams, $http) {
+    .controller('RoomController', ['$rootScope', '$scope', '$state', '$stateParams', '$http', '$timeout', function ($rootScope, $scope, $state, $stateParams, $http, $timeout) {
         $scope.gameName = $stateParams.name;
 
         $http.get('/rest/game/' + $scope.gameName)
             .then(function (data) {
                 $scope.game = data;
                 console.log($scope.game);
+
+                request();
             }, function (error) {
                 console.log(error);
             });
 
+        function request() {
+            $timeout(function() {
+                $http.get('/rest/game/' + $scope.gameName)
+                    .then(function (data) {
+                        $scope.game = data;
+                        console.log($scope.game);
+                    }, function (error) {
+                        console.log(error);
+                    });
+                request();
+            }, 5000)
+        }
     }]);
