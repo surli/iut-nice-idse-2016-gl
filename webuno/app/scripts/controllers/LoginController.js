caf1fb7feb2ccec511a8cc75add2333caacc1094
jeremy <jeremyfroment@yahoo.fr>
2016-02-08 23:02:17 +0100

    LPSUA-74 : correction bug requete post

--- webuno/app/scripts/controllers/LoginController.js
+++ webuno/app/scripts/controllers/LoginController.js
@@ -12,9 +12,7 @@ angular.module('unoApp')
 
         $scope.goLoginGuess = function () {
             var name = 'Anonyme' + Math.floor((Math.random() * (1000 - 1) + 1));
-            Auth.setUser({
-                name: name
-            }).then(function(data) {
+            Auth.setUser(name).then(function(data) {
                 data.name = name;
                 Auth.connectUser(data);
                 $state.go('app.home');
