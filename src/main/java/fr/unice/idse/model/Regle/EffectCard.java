4da081fd406af04a04851a4c26584831a6ce6cf9
Tom Phily <tom.phily@live.fr>
2016-02-08 11:22:50 +0100

    LPSUA-49 : Ajout de methode dont une à override dans classe EffectCard

--- src/main/java/fr/unice/idse/model/Regle/EffectCard.java
+++ src/main/java/fr/unice/idse/model/Regle/EffectCard.java
@@ -1,6 +1,40 @@
 package fr.unice.idse.model.Regle;
 
+import fr.unice.idse.model.*;
+
 public class EffectCard 
 {
-
+	private int value;
+	private Color color;
+	
+	public EffectCard(int value, Color color)
+	{
+		this.value = value;
+		this.color = color;
+	}
+	
+	public int getValue()
+	{
+		return value;
+	}
+	
+	public Color getColor()
+	{
+		return color;
+	}
+	
+	/**
+	 * Pour savoir si la carte jouée correspond à la règle.
+	 * @param card
+	 * @return
+	 */
+	public boolean isEffect(Card card)
+	{
+		return card.getColor().equals(getColor()) && card.getValue() == getValue();
+	}
+	
+	public void action()
+	{
+		
+	}
 }
--- src/main/java/fr/unice/idse/model/Regle/RuleDrawTwo.java
+++ src/main/java/fr/unice/idse/model/Regle/RuleDrawTwo.java
@@ -1,9 +1,17 @@
 package fr.unice.idse.model.Regle;
 
+import fr.unice.idse.model.Color;
+
 public class RuleDrawTwo extends EffectCard 
 {
 
-	public RuleDrawTwo() 
+	public RuleDrawTwo(int value, Color color) 
+	{
+		super(value, color);
+	}
+	
+	@Override
+	public void action()
 	{
 		
 	}
