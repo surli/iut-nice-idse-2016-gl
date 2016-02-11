3e548eb4348d1d3d951cc31167d5523de03d5c8c
Pierre Echardour <pierre.echardour.2013@gmail.com>
2016-02-11 15:43:13 +0100

    changement build bdd sous mysql

--- src/main/java/fr/unice/idse/db/buildingDb.sql
+++ src/main/java/fr/unice/idse/db/buildingDb.sql
@@ -1,83 +1,142 @@
-/* Creation types and tables */
+-- phpMyAdmin SQL Dump
+-- version 4.2.7.1
+-- http://www.phpmyadmin.net
+--
+-- Client :  localhost
+-- Généré le :  Jeu 11 Février 2016 à 14:39
+-- Version du serveur :  5.6.20-log
+-- Version de PHP :  5.5.15
 
-CREATE user_t AS OBJECT (
-u_pseudo VARCHAR2(20), 
-u_email VARCHAR2(30) unique, 
-u_password VARCHAR2(64),
-MEMBER FUNCTION get_uemail RETURN VARCHAR, 
-MEMBER FUNCTION get_upseudo RETURN VARCHAR
-) NOT FINAL ;
-/
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET time_zone = "+00:00";
 
-CREATE TABLE users OF user_t (
-CONSTRAINT uc1 PRIMARY KEY(u_pseudo),
-CONSTRAINT uc3 u_email NOT NULL,
-CONSTRAINT uc2 u_password NOT NULL
-);
-/
 
-CREATE SEQUENCE users_seq
-START WITH 1
-INCREMENT BY 1;
-/
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8 */;
 
---CREATE OR REPLACE TRIGGER users_trigger
---BEFORE INSERT
---ON users
---REFERENCING NEW AS NEW
---FOR EACH ROW
---BEGIN
---SELECT users_seq.NEXTVAL INTO :NEW.u_id FROM dual;
---END;
---/
+--
+-- Base de données :  `uno`
+--
 
-CREATE TYPE BODY user_t AS 
-MEMBER FUNCTION get_uemail RETURN VARCHAR AS
-BEGIN RETURN u_email; END;
-MEMBER FUNCTION get_upseudo RETURN VARCHAR AS
-BEGIN RETURN u_pseudo; END;
-END;
-/
+-- --------------------------------------------------------
 
-CREATE TYPE card_t AS OBJECT (
-h_value VARCHAR2(10), 
-h_color VARCHAR2(10)
-) ;
-/
+--
+-- Structure de la table `cards`
+--
 
-CREATE TYPE playerslist_t AS VARRAY(4) OF VARCHAR(20);
-/
-CREATE TYPE hand_t AS VARRAY(20) OF card_t;
-/
-CREATE TYPE handslist_t AS VARRAY(4) OF hand_t;
-/
+CREATE TABLE IF NOT EXISTS `cards` (
+`c_id` int(5) NOT NULL,
+  `c_value` varchar(20) NOT NULL,
+  `c_color` varchar(20) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 AUTO_INCREMENT=1 ;
 
-CREATE game_t AS OBJECT (
-g_name VARCHAR(50),
-g_players playerslist_t, 
-g_hands handslist_t
-) ;
-/
+-- --------------------------------------------------------
 
-CREATE TABLE games OF game_t (
-CONSTRAINT gc1 PRIMARY KEY(g_name),
-CONSTRAINT gc2 g_players NOT NULL,
-CONSTRAINT gc3 g_hands NOT NULL
-);
-/
+--
+-- Structure de la table `games`
+--
 
-CREATE SEQUENCE games_seq
-START WITH 1
-INCREMENT BY 1;
-/
+CREATE TABLE IF NOT EXISTS `games` (
+`g_id` int(5) NOT NULL,
+  `g_id_players_list` int(5) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 AUTO_INCREMENT=1 ;
 
---CREATE OR REPLACE TRIGGER games_trigger
---BEFORE INSERT
---ON games
---REFERENCING NEW AS NEW
---FOR EACH ROW
---BEGIN
---SELECT games_seq.NEXTVAL INTO :NEW.g_id FROM dual;
---END;
---/
+-- --------------------------------------------------------
 
+--
+-- Structure de la table `hands_players_in_game`
+--
+
+CREATE TABLE IF NOT EXISTS `hands_players_in_game` (
+  `h_id_game` int(5) NOT NULL,
+  `h_id_player` int(5) NOT NULL,
+  `h_id_card` int(5) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `players_in_game`
+--
+
+CREATE TABLE IF NOT EXISTS `players_in_game` (
+  `p_id` int(5) NOT NULL,
+  `p_id_player` int(5) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `users`
+--
+
+CREATE TABLE IF NOT EXISTS `users` (
+`u_id` int(5) NOT NULL,
+  `u_pseudo` varchar(20) NOT NULL,
+  `u_email` varchar(50) NOT NULL,
+  `u_password` varchar(40) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 AUTO_INCREMENT=1 ;
+
+--
+-- Index pour les tables exportées
+--
+
+--
+-- Index pour la table `cards`
+--
+ALTER TABLE `cards`
+ ADD PRIMARY KEY (`c_id`), ADD KEY `c_id` (`c_id`);
+
+--
+-- Index pour la table `games`
+--
+ALTER TABLE `games`
+ ADD PRIMARY KEY (`g_id`);
+
+--
+-- Index pour la table `hands_players_in_game`
+--
+ALTER TABLE `hands_players_in_game`
+ ADD KEY `h_id_card` (`h_id_card`), ADD KEY `h_id_player` (`h_id_player`), ADD KEY `h_id_game` (`h_id_game`);
+
+--
+-- Index pour la table `users`
+--
+ALTER TABLE `users`
+ ADD PRIMARY KEY (`u_id`,`u_pseudo`,`u_email`);
+
+--
+-- AUTO_INCREMENT pour les tables exportées
+--
+
+--
+-- AUTO_INCREMENT pour la table `cards`
+--
+ALTER TABLE `cards`
+MODIFY `c_id` int(5) NOT NULL AUTO_INCREMENT;
+--
+-- AUTO_INCREMENT pour la table `games`
+--
+ALTER TABLE `games`
+MODIFY `g_id` int(5) NOT NULL AUTO_INCREMENT;
+--
+-- AUTO_INCREMENT pour la table `users`
+--
+ALTER TABLE `users`
+MODIFY `u_id` int(5) NOT NULL AUTO_INCREMENT;
+--
+-- Contraintes pour les tables exportées
+--
+
+--
+-- Contraintes pour la table `hands_players_in_game`
+--
+ALTER TABLE `hands_players_in_game`
+ADD CONSTRAINT `hands_players_in_game_ibfk_1` FOREIGN KEY (`h_id_card`) REFERENCES `cards` (`c_id`) ON DELETE CASCADE ON UPDATE CASCADE,
+ADD CONSTRAINT `hands_players_in_game_ibfk_2` FOREIGN KEY (`h_id_game`) REFERENCES `games` (`g_id`) ON DELETE CASCADE ON UPDATE CASCADE;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
