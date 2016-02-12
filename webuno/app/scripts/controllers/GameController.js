f8d38c53805d233c000a2657fc219b1ad433cbcb
jeremy <jeremyfroment@yahoo.fr>
2016-02-12 14:57:58 +0100

    LPSUA-100 : Création des joueurs dans le game (pas encore de design) LPSUA-113 : Ajout de la fonctionnalité pour piocher une carte LPSUA-114 : Ajout de la fonctionnalité pour poser une carte

--- webuno/app/scripts/controllers/GameController.js
+++ webuno/app/scripts/controllers/GameController.js
@@ -15,8 +15,7 @@ angular.module('unoApp')
         // TODO remplacer par Game.getGame(name)
         $http.get('/rest/game/' + $stateParams.name)
             .then(function (response) {
-                console.log("Game : ", response.data);
-                $scope.fausse = response.data.stack;
+                $scope.game = response.data;
                 $scope.requestStateGame();
             }, function (error) {
                 console.error('Une erreur est survenue : ' + error.toString());
@@ -28,13 +27,21 @@ angular.module('unoApp')
                 //TODO remplacer par Game.getGame (attention il y a un traitement en plus que faire ?)
                 $http.get('/rest/game/' + $stateParams.name)
                     .then(function (response) {
-                        console.log("Game : ", response.data);
-                        $scope.fausse = response.data.stack;
+                        $scope.game = response.data;
+
+                        $http.get('/rest/game/' + $stateParams.name + '/command')
+                            .then(function (response) {
+                                if (response.data.playerName === $scope.user.name) {
+                                    console.log('à moi de jouer !');
+                                }
+                            }, function (error) {
+                                console.error(error);
+                            });
+
                         $scope.requestStateGame();
 
                         $http.get('/rest/game/' + $stateParams.name + '/' + $scope.user.name)
                             .then(function (response) {
-                                console.log("Cartes : ", response.data.cartes);
                                 $scope.cartes = response.data.cartes;
                             }, function (error) {
                                 console.error('Une erreur est survenue : ' + error.toString());
@@ -47,12 +54,41 @@ angular.module('unoApp')
         };
 
         $scope.piocherCarte = function () {
-            $scope.cartes.push($scope.cartes[$scope.cartes.length % 8]);
+            $http.post('/rest/game/' + $stateParams.name + '/' + $scope.user.name, {})
+                .then(function (response) {
+                    $http.get('/rest/game/' + $stateParams.name + '/' + $scope.user.name)
+                        .then(function (response) {
+                            $scope.cartes = response.data.cartes;
+                        }, function (error) {
+                            console.error('Une erreur est survenue : ' + error.toString());
+                        });
+                }, function (error) {
+                    console.error(error);
+                });
         };
 
         $scope.jouerCarte = function (carte) {
-            $scope.cartes.splice($scope.cartes.indexOf(carte), 1);
-            $scope.fausse = carte;
+            $http.put('/rest/game/' + $stateParams.name + '/' + $scope.user.name, {
+                    value: carte.number,
+                    color: carte.familly
+                })
+                .then(function (response) {
+                    console.log(response);
+                    $http.get('/rest/game/' + $stateParams.name + '/' + $scope.user.name)
+                        .then(function (response) {
+                            $scope.cartes = response.data.cartes;
+                        }, function (error) {
+                            console.error('Une erreur est survenue : ' + error.toString());
+                        });
+                    $http.get('/rest/game/' + $stateParams.name)
+                        .then(function (response) {
+                            $scope.game = response.data;
+                        }, function (error) {
+                            console.error('Une erreur est survenue : ' + error.toString());
+                        });
+                }, function (error) {
+                    console.error(error);
+                });
         };
 
         $scope.$on('$destroy', function () {
