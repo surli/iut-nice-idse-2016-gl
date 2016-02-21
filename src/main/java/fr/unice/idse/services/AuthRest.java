8e633a260b9a1f77afa3da63b92bf92277eb8edd
Jeremie Elbaz <jeremie-e@hotmail.fr>
2016-02-21 19:37:45 +0100

    LPSUA-146 : Creation de la methode pour se connecter avec ses identifiants. En attente de la bdd

--- src/main/java/fr/unice/idse/services/AuthRest.java
+++ src/main/java/fr/unice/idse/services/AuthRest.java
@@ -5,7 +5,9 @@ import fr.unice.idse.model.Model;
 import org.codehaus.jettison.json.JSONException;
 import org.codehaus.jettison.json.JSONObject;
 
+import javax.smartcardio.ResponseAPDU;
 import javax.ws.rs.POST;
+import javax.ws.rs.PUT;
 import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
@@ -24,7 +26,7 @@ public class AuthRest extends OriginRest{
      */
     @POST
     @Produces(MediaType.APPLICATION_JSON)
-    public Response authPlayerGuest(String stringJson) throws JSONException, InvalidKeySpecException, NoSuchAlgorithmException {
+    public Response authPlayerGuest(String stringJson) throws JSONException {
         Model model = Model.getInstance();
         JSONObject jsonObject = new JSONObject(stringJson);
         JSONObject jsonReturn = new JSONObject();
@@ -45,4 +47,41 @@ public class AuthRest extends OriginRest{
         jsonReturn.put("error", "Player already exist");
         return sendResponse(405, jsonReturn.toString(), "POST");
     }
+
+    /**
+     * Authentifie un utilisateur avec ses identifiants
+     * @param json String
+     * @return Response
+     * @throws JSONException
+     */
+    @PUT
+    @Produces(MediaType.APPLICATION_JSON)
+    public Response authPlayer(String json) throws JSONException {
+        JSONObject jsonObject = new JSONObject(json);
+        JSONObject jsonResult = new JSONObject();
+        Model model = Model.getInstance();
+
+        if(!jsonObject.has("email")){
+            jsonResult.put("error", "Missing parameter email");
+            return sendResponse(405, jsonResult.toString(), "PUT");
+        }
+
+        if(!jsonObject.has("password")){
+            jsonResult.put("error", "Missing parameter password");
+            return sendResponse(405, jsonResult.toString(), "PUT");
+        }
+
+        
+
+        String token = generateToken(jsonObject.getString("email"));
+        if(model.createPlayer("", token)){
+            jsonResult.put("token", token);
+            return sendResponse(200, jsonResult.toString(), "PUT");
+        }
+
+        jsonResult.put("error", "Player already exist");
+        return sendResponse(405, jsonResult.toString(), "PUT");
+    }
+
+
 }
