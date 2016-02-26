8483ee182991ed7dec46e7565984eeb9144fb7f5
jeremy <jeremyfroment@yahoo.fr>
2016-02-26 17:51:47 +0100

    LPSUA-185 : Ajout de junit dans le reporter des tests

--- webuno/test/karma.conf.js
+++ webuno/test/karma.conf.js
@@ -35,7 +35,7 @@ module.exports = function(config) {
       'bower_components/angular-mocks/angular-mocks.js',
       // endbower
       "app/scripts/**/*.js",
-      "test/mock/**/*.js",
+      //"test/mock/**/*.js",
       "test/spec/**/*.js"
     ],
 
@@ -44,7 +44,7 @@ module.exports = function(config) {
     ],
 
     // web server port
-    port: 8080,
+    port: 9012,
 
     // Start these browsers, currently available:
     // - Chrome
@@ -66,13 +66,13 @@ module.exports = function(config) {
 
     // Continuous Integration mode
     // if true, it capture browsers, run tests and exit
-    singleRun: false,
-
+    singleRun: true,
+    reporters: ['dots', 'junit'],
     colors: true,
 
     // level of logging
     // possible values: LOG_DISABLE || LOG_ERROR || LOG_WARN || LOG_INFO || LOG_DEBUG
-    logLevel: config.LOG_INFO,
+    logLevel: config.LOG_INFO
 
     // Uncomment the following lines if you are using grunt's server to run the tests
     // proxies: {
