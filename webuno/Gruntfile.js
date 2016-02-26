f16e394929df514bca55466d1831169762a1c9a0
jeremy <jeremyfroment@yahoo.fr>
2016-02-26 17:18:52 +0100

    LPSUA-185 : modification de la destination des fonts dans le grunt build

--- webuno/Gruntfile.js
+++ webuno/Gruntfile.js
@@ -434,14 +434,9 @@ module.exports = function (grunt) {
                     src: ['generated/*']
                 }, {
                     expand: true,
-                    cwd: '.',
-                    src: 'bower_components/bootstrap-sass-official/assets/fonts/bootstrap/*',
-                    dest: '<%= yeoman.dist %>'
-                }, {
-                    expand: true,
-                    cwd: '.',
-                    src: 'bower_components/components-font-awesome/css/font-awesome.css',
-                    dest: '<%= yeoman.dist %>'
+                    cwd: 'bower_components/bootstrap-sass-official/assets/fonts/bootstrap/',
+                    src: ['**'],
+                    dest: '<%= yeoman.dist %>/fonts'
                 }, {
                     expand: true,
                     cwd: 'bower_components/components-font-awesome/fonts/',
