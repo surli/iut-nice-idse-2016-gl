e24013e30629ed43b73912e58b3475a04af0f6dd
Kévin Detti <kevin.detti@hotmail.fr>
2016-03-20 16:56:58 +0100

    LPSUA-221 : Tests des méthodes clearStack, topCard et changeColor

--- src/test/java/model/StackTest.java
+++ src/test/java/model/StackTest.java
@@ -14,10 +14,17 @@ public class StackTest {
 
 	Stack stack;
 	Deck deck;
+	Card oneBlue, twoRed, threeYellow, fourGreen, drawTwo, wild;
 	
 	@Before
 	public void initialize(){
 		stack = new Stack(); 
+		oneBlue = new Card(Value.One, Color.Blue);
+		twoRed = new Card(Value.Two, Color.Red);
+		threeYellow = new Card(Value.Three, Color.Yellow);
+		fourGreen = new Card(Value.Four, Color.Green);
+		drawTwo = new Card(Value.DrawTwo, Color.Black);
+		wild = new Card(Value.Wild, Color.Black);
 	}
 
 	@Test
@@ -27,9 +34,10 @@ public class StackTest {
 	
 	@Test
 	public void methodeCountCardsRenvoieLeBonNombreDeCartes(){
-		stack.addCard(new Card(Value.One,Color.Green));
-		stack.addCard(new Card(Value.Four,Color.Red));
-		stack.addCard(new Card(Value.Six,Color.Yellow));
+		stack.addCard(oneBlue);
+		stack.addCard(twoRed);
+		stack.addCard(threeYellow);
+		stack.addCard(drawTwo);
 		
 		assertEquals(stack.countCards(),stack.getStack().size());
 	}
@@ -42,8 +50,33 @@ public class StackTest {
 	
 	@Test
 	public void methodeIsEmptyRenvoieFauxSiLeTalonNEstPasVide(){
-		stack.addCard(new Card(Value.Four,Color.Red));
-		stack.addCard(new Card(Value.Six,Color.Yellow));
+		stack.addCard(oneBlue);
+		stack.addCard(twoRed);
 		assertFalse(stack.isEmpty());
+	}
+	
+	@Test
+	public void methodeClearStackVideLeTalon(){
+		stack.addCard(threeYellow);
+		stack.addCard(drawTwo);
+		stack.clearStack();
+		assertTrue(stack.isEmpty());
+	}
+	
+	@Test
+	public void topCardRetourneLaCarteSuperieure(){
+		stack.addCard(oneBlue);
+		stack.addCard(twoRed);
+		stack.addCard(threeYellow);
+		stack.addCard(drawTwo);
+		assertEquals(drawTwo, stack.topCard());
+	}
+	
+	@Test
+	public void changeColorChangeBienLaCouleurDeLaCarteSuperieure(){
+		stack.addCard(oneBlue);
+		stack.addCard(twoRed);
+		stack.changeColor(Color.Yellow);
+		assertEquals(Color.Yellow, stack.topCard().getColor());
 	}
 }
