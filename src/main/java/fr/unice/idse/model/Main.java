2a29871d07f5d0bb56b5bc53b03a928b650312d2
Régis Parpex <regis.parpex@gmail.com>
2016-03-20 19:51:40 +0100

    Stabilisation Tests LPSUA-262 : [Moteur] Ajout des tests IA (reverted from commit bf48af00a90abc93345dfe7c0d16bc823bcf747d)

--- src/main/java/fr/unice/idse/model/Main.java
+++ src/main/java/fr/unice/idse/model/Main.java
@@ -1,4 +1,5 @@
 package fr.unice.idse.model;
+
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
@@ -9,18 +10,20 @@ import fr.unice.idse.model.card.Color;
 import fr.unice.idse.model.regle.EffectCard;
 import fr.unice.idse.model.regle.RuleChangeColor;
 import fr.unice.idse.model.player.*;
+
+
 public class Main
 {
+
     public static void main(String[] args)
     {
         Player playerHostTest = new Player("PlayerHostTest","tok1");
-        IAEasy playerTest2 = new IAEasy("Test","tok2",1);
-//      IA playerTest3 = new IA("Toto","tok3",2);
-//      IA playerTest4 = new IA("Toto","tok3",3);
+        Player playerTest2 = new Player("Test","tok2");
+        Player playerTest3 = new Player("Toto","tok3");
         Game gameTest = new Game(playerHostTest, "GameTest", 3);
+
         gameTest.addPlayer(playerTest2);
-//      gameTest.addPlayer(playerTest3);
-//      gameTest.addPlayer(playerTest4);
+        gameTest.addPlayer(playerTest3);
         if (gameTest.start())
         {
             Board board = gameTest.getBoard();
@@ -45,120 +48,96 @@ public class Main
                     boolean played = false;
                     while(!played)
                     {
-                        if(actualPlayer instanceof IAEasy) {
-                        	((IAEasy) actualPlayer).reflexion(board);
-                            //IA Reflexion
-                        	if(gameTest.gameEnd())
+                        int numberCardsPlayer = actualPlayer.getCards().size();
+                        System.out.println("Entrez une valeure qui est entre crochet pour jouer une carte.");
+                        BufferedReader bufferRead = new BufferedReader(new InputStreamReader(System.in));
+                        String input = "";
+                        try
+                        {
+                            input = bufferRead.readLine();
+                        }
+                        catch (IOException e)
+                        {
+                            e.printStackTrace();
+                        }
+                        int numberCard = Integer.parseInt(input);
+                        if(input != "" && numberCard < actualPlayer.getCards().size())
+                        {
+
+                            Card card = actualPlayer.getCards().get(numberCard);
+                            board.poseCard(card);
+                            if(gameTest.gameEnd())
                             {
                                 winner = actualPlayer;
                                 played = true;
-                                //System.out.println("Le joueur joue : " + card);
+                                System.out.println("Le joueur joue : " + card);
                             }
                             else
                             {
-                            played = true;
-                            }
-                        }
-                        else {
-                        	
-                        	int numberCardsPlayer = actualPlayer.getCards().size();
-                            System.out.println("Entrez une valeur qui est entre crochet pour jouer une carte.");
-                            BufferedReader bufferRead = new BufferedReader(new InputStreamReader(System.in));
-                            String input = "";
-                            try
-                            {
-                                input = bufferRead.readLine();
-                            }
-                            catch (IOException e)
-                            {
-                                e.printStackTrace();
-                            }
-                            int numberCard = Integer.parseInt(input);
-                            if(!input.equals("") && numberCard < actualPlayer.getCards().size())
-                            {
-                                Card card = actualPlayer.getCards().get(numberCard);
-                                board.poseCard(card);
-                                if(gameTest.gameEnd())
+                                if(numberCardsPlayer > actualPlayer.getCards().size())
                                 {
-                                    winner = actualPlayer;
                                     played = true;
                                     System.out.println("Le joueur joue : " + card);
-                                }
-                                else
-                                {
-                                    if(numberCardsPlayer > actualPlayer.getCards().size())
+                                    if(actualPlayer.getCards().size() == 1)
                                     {
-                                        played = true;
-                                        System.out.println("Le joueur joue : " + card);
-                                        if(actualPlayer.getCards().size() == 1)
+                                        System.out.println("Uno");
+                                    }
+                                    EffectCard effectCard = variante.getEffectCard(card);
+                                    if(effectCard != null)
+                                    {
+                                        if(card.getColor().equals(Color.Black))
                                         {
-                                            System.out.println("Uno");
-                                        }
-                                        EffectCard effectCard = variante.getEffectCard(card);
-                                        if(effectCard != null)
-                                        {
-                                            if(card.getColor().equals(Color.Black))
+                                            boolean chooseColor = false;
+                                            while (!chooseColor)
                                             {
-                                                boolean chooseColor = false;
-                                                while (!chooseColor)
+                                                System.out.println("Choisie une couleur :  1 pour Red, 2 pour Blue, 3 pour Yellow, 4 pour Green");
+                                                input = "";
+                                                try
                                                 {
-                                                    int choose = 0;
-                                                    if(actualPlayer instanceof IA) {
-                                                        //int choose = actualPlayer reflexion
-                                                        choose = 1;
-                                                    }
-                                                    else {
-                                                        System.out.println("Choisie une couleur :  1 pour Red, 2 pour Blue, 3 pour Yellow, 4 pour Green");
-                                                        input = "";
-                                                        try
-                                                        {
-                                                            input = bufferRead.readLine();
-                                                        }
-                                                        catch (IOException e)
-                                                        {
-                                                            e.printStackTrace();
-                                                        }
-                                                        choose = Integer.parseInt(input);
-                                                    }
-                                                    switch(choose)
-                                                    {
-                                                        case 1 :
-                                                            effectCard.changeColor(Color.Red);
-                                                            chooseColor = true;
-                                                            System.out.println("Le joueur a choisie : Red");
-                                                            break;
-                                                        case 2 :
-                                                            effectCard.changeColor(Color.Blue);
-                                                            System.out.println("Le joueur a choisie : Blue");
-                                                            chooseColor = true;
-                                                            break;
-                                                        case 3 :
-                                                            effectCard.changeColor(Color.Yellow);
-                                                            System.out.println("Le joueur a choisie : Yellow");
-                                                            chooseColor = true;
-                                                            break;
-                                                        case 4 :
-                                                            effectCard.changeColor(Color.Green);
-                                                            System.out.println("Le joueur a choisie : Green");
-                                                            chooseColor = true;
-                                                            break;
-                                                        default :
-                                                            break;
-                                                    }
+                                                    input = bufferRead.readLine();
+                                                }
+                                                catch (IOException e)
+                                                {
+                                                    e.printStackTrace();
+                                                }
+                                                int choose = Integer.parseInt(input);
+                                                switch(choose)
+                                                {
+                                                    case 1 :
+                                                        effectCard.changeColor(Color.Red);
+                                                        chooseColor = true;
+                                                        System.out.println("Le joueur a choisie : Red");
+                                                        break;
+                                                    case 2 :
+                                                        effectCard.changeColor(Color.Blue);
+                                                        System.out.println("Le joueur a choisie : Blue");
+                                                        chooseColor = true;
+                                                        break;
+                                                    case 3 :
+                                                        effectCard.changeColor(Color.Yellow);
+                                                        System.out.println("Le joueur a choisie : Yellow");
+                                                        chooseColor = true;
+                                                        break;
+                                                    case 4 :
+                                                        effectCard.changeColor(Color.Green);
+                                                        System.out.println("Le joueur a choisie : Green");
+                                                        chooseColor = true;
+                                                        break;
+                                                    default :
+                                                        break;
                                                 }
                                             }
-                                            effectCard.action();
                                         }
-                                        board.nextPlayer();
-                                        if(effectCard != null && effectCard.getEffect())
-                                        {
-                                            effectCard.effect();
-                                        }
+                                        effectCard.action();
+                                    }
+                                    board.nextPlayer();
+                                    if(effectCard != null && effectCard.getEffect())
+                                    {
+                                        effectCard.effect();
                                     }
                                 }
                             }
                         }
-                        
                     }
                 }
                 else
@@ -170,6 +149,7 @@ public class Main
                         board.nextPlayer();
                     }
                 }
+
             }
             System.out.println("Fin de la partie. Vainqueur : " + winner.getName());
         }
--- src/test/java/model/PlayerTest.java
+++ src/test/java/model/PlayerTest.java
@@ -12,17 +12,14 @@ import fr.unice.idse.model.player.Player;
 import fr.unice.idse.model.card.Card;
 import fr.unice.idse.model.card.Color;
 import fr.unice.idse.model.card.Value;
-import fr.unice.idse.model.player.*;
 
 public class PlayerTest {
 
 	private Player player;
-	private IAEasy player2;
 	
 	@Before
 	public void initialize(){
 		player = new Player("test","");
-		IAEasy player2 = new IAEasy("Test","tok2",1);
 		
 		ArrayList<Card> cards=new ArrayList<Card>();
 		
@@ -32,15 +29,9 @@ public class PlayerTest {
 		cards.add(new Card(Value.Eight, Color.Blue));
 		cards.add(new Card(Value.Six, Color.Blue));
 		cards.add(new Card(Value.Two, Color.Green));
-		
-		ArrayList<Card> mainIA = new ArrayList<Card>();
-		
-		mainIA.add(new Card(Value.Two, Color.Blue));
 
 		player.setCards(cards);
 		player.sortCards();
-		player2.setCards(mainIA);
-		
 	}
 
 	@Test
@@ -57,13 +48,22 @@ public class PlayerTest {
 		
 		assertEquals(expected, player.getCards());
 	}
-	
+
+/*
 	@Test
 	public void  testDuChangeColorDuJoueur()
 	{
+		IAEasy monIA = new IAEasy("Test","tok",1);
+		ArrayList<Card> mainIA = new ArrayList<Card>();
+
+		mainIA.add(new Card(Value.Two, Color.Blue));
+		monIA.setCards(mainIA);
+
 		Color colorExpected = Color.Blue;
 		
-		assertEquals(colorExpected, IAEasy.chooseColor(player2.getCards()));
+		assertEquals(colorExpected, IAEasy.chooseColor(monIA.getCards()));
 		
 	}
+
+	*/
 }
