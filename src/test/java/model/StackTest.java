bc1a426b81a8c17487bfc4e2a9df29af82dcc5e5
Kévin Detti <kevin.detti@hotmail.fr>
2016-03-20 16:38:03 +0100

    LPSUA-221 : Tests des méthodes isEmpty et countCards

--- src/test/java/model/StackTest.java
+++ src/test/java/model/StackTest.java
@@ -2,13 +2,48 @@ package model;
 
 import static org.junit.Assert.*;
 
+import org.junit.Before;
 import org.junit.Test;
+
+import fr.unice.idse.model.*;
+import fr.unice.idse.model.card.Card;
+import fr.unice.idse.model.card.Color;
+import fr.unice.idse.model.card.Value;
 
 public class StackTest {
 
-	@Test
-	public void test() {
-		fail("Not yet implemented");
+	Stack stack;
+	Deck deck;
+	
+	@Before
+	public void initialize(){
+		stack = new Stack(); 
 	}
 
+	@Test
+	public void talonInitialiseEstVide(){
+		assertTrue(stack.isEmpty());
+	}
+	
+	@Test
+	public void methodeCountCardsRenvoieLeBonNombreDeCartes(){
+		stack.addCard(new Card(Value.One,Color.Green));
+		stack.addCard(new Card(Value.Four,Color.Red));
+		stack.addCard(new Card(Value.Six,Color.Yellow));
+		
+		assertEquals(stack.countCards(),stack.getStack().size());
+	}
+	
+	@Test
+	public void methodeIsEmptyRenvoieVraiSiLeTalonEstVide(){
+		stack.clearStack();
+		assertTrue(stack.isEmpty());
+	}
+	
+	@Test
+	public void methodeIsEmptyRenvoieFauxSiLeTalonNEstPasVide(){
+		stack.addCard(new Card(Value.Four,Color.Red));
+		stack.addCard(new Card(Value.Six,Color.Yellow));
+		assertFalse(stack.isEmpty());
+	}
 }
