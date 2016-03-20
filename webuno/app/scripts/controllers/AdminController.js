be8cd0ca96b388ab46c0d94ae5d1d1e4dfa04945
jeremy <jeremyfroment@yahoo.fr>
2016-03-20 17:30:01 +0100

    LPSUA-239 : - Ajout d'un piechart pour les stats

--- webuno/app/scripts/controllers/AdminController.js
+++ webuno/app/scripts/controllers/AdminController.js
@@ -12,5 +12,34 @@ angular.module('unoApp')
 
         Game.getAllGames(function (data) {
             $scope.games = data.games;
+            console.log($scope.games);
+
+            google.charts.load('current', {'packages':['corechart']});
+            google.charts.setOnLoadCallback(drawChart);
         });
+
+        function drawChart() {
+            var games = [
+                ['Statut', 'Nombre de parties'],
+                ['En attente de joueurs', 0],
+                ['Complète' , 0],
+                ['Demarrée' , 0],
+                ['Terminée' , 0]
+            ];
+
+            angular.forEach($scope.games, function(game) {
+                if (game.state) {
+                    games[3][1]++;
+                } else {
+                    if (game.numberplayer === game.maxplayer) {
+                        games[2][1]++;
+                    } else {
+                        games[1][1]++;
+                    }
+                }
+            });
+
+            var chart = new google.visualization.PieChart(document.getElementById('piechart'));
+            chart.draw(google.visualization.arrayToDataTable(games), {});
+        }
     }]);
