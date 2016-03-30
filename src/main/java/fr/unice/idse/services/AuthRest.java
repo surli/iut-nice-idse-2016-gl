6fa73ab35a3e07ffd5dba3ff3b2e7cf21e9e051a
Damien Clemenceau <damien.clemenceau@outlook.com>
2016-03-30 18:42:25 +0200

    LPSUA-298 : Clean du pom.xml

--- src/main/java/fr/unice/idse/services/AuthRest.java
+++ src/main/java/fr/unice/idse/services/AuthRest.java
@@ -1,17 +1,21 @@
 package fr.unice.idse.services;
 
-import fr.unice.idse.db.DataBaseManagement;
-import fr.unice.idse.model.Model;
+import java.util.regex.Pattern;
+
+import javax.ws.rs.DELETE;
+import javax.ws.rs.HeaderParam;
+import javax.ws.rs.POST;
+import javax.ws.rs.PUT;
+import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.Response;
+
 import org.codehaus.jettison.json.JSONException;
 import org.codehaus.jettison.json.JSONObject;
 
-import javax.smartcardio.ResponseAPDU;
-import javax.ws.rs.*;
-import javax.ws.rs.core.MediaType;
-import javax.ws.rs.core.Response;
-import java.security.NoSuchAlgorithmException;
-import java.security.spec.InvalidKeySpecException;
-import java.util.regex.Pattern;
+import fr.unice.idse.db.DataBaseManagement;
+import fr.unice.idse.model.Model;
 
 @Path("auth")
 public class AuthRest extends OriginRest{
