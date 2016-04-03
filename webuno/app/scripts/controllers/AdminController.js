d9656e7db57f848d8c65888e152322fef0cce47b
jeremy <jeremyfroment@yahoo.fr>
2016-04-03 16:39:59 +0200

    LPSUA-306 : Fix bug instanciation google chart

--- webuno/app/scripts/controllers/AdminController.js
+++ webuno/app/scripts/controllers/AdminController.js
@@ -5,7 +5,7 @@ angular.module('unoApp')
      * Contrôleur global AppController de la route /app
      * Gère l'ensemble de l'application une fois un utilisateur connecté
      */
-    .controller('AdminController', ['$rootScope', '$scope', '$state', 'Auth', 'Game', function ($rootScope, $scope, $state, Auth, Game) {
+    .controller('AdminController', ['$rootScope', '$scope', '$state', '$timeout', 'Auth', 'Game', function ($rootScope, $scope, $state, $timeout, Auth, Game) {
         if (!Auth.isAdmin()) {
             $state.go('login');
         }
@@ -13,37 +13,45 @@ angular.module('unoApp')
         function initAdmin() {
             Game.getAllGamesAdmin(function (data) {
                 $scope.games = data.games;
-                console.log($scope.games);
 
-                google.charts.load('current', {'packages': ['corechart']});
-                google.charts.setOnLoadCallback(function () {
-                    var games = [
-                        ['Statut', 'Nombre de parties'],
-                        ['En attente de joueurs', 0],
-                        ['Complète', 0],
-                        ['Demarrée', 0],
-                        ['Terminée', 0]
-                    ];
+                $timeout(function() {
+                    google.charts.setOnLoadCallback(function () {
+                        var games = [
+                            ['Statut', 'Nombre de parties'],
+                            ['En attente de joueurs', 0],
+                            ['Complète', 0],
+                            ['Demarrée', 0],
+                            ['Terminée', 0]
+                        ];
 
-                    angular.forEach($scope.games, function (game) {
-                        if (game.state) {
-                            games[3][1]++;
-                        } else {
-                            if (game.numberplayer === game.maxplayer) {
-                                games[2][1]++;
+                        angular.forEach($scope.games, function (game) {
+                            if (game.state) {
+                                games[3][1]++;
                             } else {
-                                games[1][1]++;
+                                if (game.numberplayer === game.maxplayer) {
+                                    games[2][1]++;
+                                } else {
+                                    games[1][1]++;
+                                }
                             }
-                        }
-                    });
+                        });
 
-                    var chart = new google.visualization.PieChart(document.getElementById('piechart'));
-                    chart.draw(google.visualization.arrayToDataTable(games), {});
-                });
+                        var chartGames = new google.visualization.PieChart(document.getElementById('piechartgames'));
+                        chartGames.draw(google.visualization.arrayToDataTable(games), {});
+                    });
+                }, 1000);
             });
         }
 
-        initAdmin();
+        //function waitForInitChart() {
+        //    if (document.getElementById('piechartgames')) {
+                initAdmin();
+            //} else {
+            //    waitForInitChart();
+            //}
+        //}
+
+        //waitForInitChart();
 
         $scope.goVisualisation = function (gameName) {
             Game.getGameAdmin(gameName, function (data) {
