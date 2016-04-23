38c61f20fd6581e6024e8943b624c8de0fe2f4c7
JoHein <jdheinen@yahoo.com>
2016-04-23 14:55:07 +0200

    LPSUA-335: Réparation de mauvais commit
    Regréssion a cause du commit de Tom

--- src/main/java/fr/unice/idse/model/save/Save.java
+++ src/main/java/fr/unice/idse/model/save/Save.java
@@ -1,5 +1,6 @@
 package fr.unice.idse.model.save;
 
+import java.sql.SQLException;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Observable;
@@ -8,7 +9,18 @@ import java.util.Observer;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-import fr.unice.idse.db.*;
+import fr.unice.idse.db.dao.CardDAO;
+import fr.unice.idse.db.dao.DAOFactory;
+import fr.unice.idse.db.dao.GameDAO;
+import fr.unice.idse.db.dao.MatchDAO;
+import fr.unice.idse.db.dao.UserDAO;
+import fr.unice.idse.db.dao.object.GameObject;
+import fr.unice.idse.db.dao.object.HandPlayerObject;
+import fr.unice.idse.db.dao.object.MatchObject;
+import fr.unice.idse.db.dao.object.PlayerObject;
+import fr.unice.idse.db.dao.object.StackObject;
+import fr.unice.idse.db.dao.object.TurnObject;
+import fr.unice.idse.db.dao.object.UserObject;
 import fr.unice.idse.model.Game;
 import fr.unice.idse.model.card.Card;
 import fr.unice.idse.model.player.Player;
@@ -17,20 +29,8 @@ public class Save implements Observer {
 	private Logger logger = LoggerFactory.getLogger(Save.class);
 	private static Save instance;
 	
-	protected DataBaseGame gameDAO;
-	protected DataBaseUser userDAO;
-	protected DataBaseCard cardDAO;	
-
 	protected Save() {
-		gameDAO = new DataBaseGame();
-		userDAO = new DataBaseUser();
-		cardDAO = new DataBaseCard();
-	}
 
-	protected Save(String connector) {
-		gameDAO = new DataBaseGame(connector);
-		userDAO = new DataBaseUser(connector);
-		cardDAO = new DataBaseCard(connector);
 	}
 
 	public static Save getInstance() {
@@ -65,62 +65,114 @@ public class Save implements Observer {
 
 	/*
 	 * Sauvegarde d'une nouvelle partie
-	 * 
 	 * @param Game game
 	 */
-	private void saveNewGame(Game game) throws Exception {
-		if(gameDAO.addGame(game)) {
-			throw new Exception("An error occured - The game could not be added");
+	private void saveNewGame(Game game) throws SQLException {
+		// Add Game
+		GameObject gameObject = new GameObject();
+		gameObject.setNom(game.getGameName());
+		gameObject.setStatus(1);
+		gameObject.setNbMaxIa(0);
+		gameObject.setNbMaxJoueurs(game.getNumberPlayers());		
+		DAOFactory.getGameDAO().create(gameObject);
+
+		// Add players
+		List<PlayerObject> players = new ArrayList<>();
+		for (int i = 0; i < game.getPlayers().size(); i++) {
+			UserObject user = ((UserDAO)DAOFactory.getUserDAO()).find(game.getPlayers().get(i).getName());
+			players.add(new PlayerObject(gameObject.getId(), user.getId(), i));
+			DAOFactory.getPlayerDAO().create(players.get(i));
 		}
+		
+		// Add match
+		MatchObject match = new MatchObject();
+		match.setIdGame(gameObject.getId());
+		DAOFactory.getMatchDAO().create(match);
 
-		List<Player> arrayPlayer = game.getPlayers();
+		// Add turn
+		TurnObject turn = new TurnObject();
+		turn.setIdMatch(match.getId());
+		turn.setIdUser(players.get(0).getIdUser());
+		turn.setInverded(game.getDirection());
+		DAOFactory.getTurnDAO().create(turn);
 
-		for (int i = 0; i < arrayPlayer.size(); i++) {
-			BusinessQuery.addPlayerToGame(game, arrayPlayer.get(i), i);
-		}
-
-		Integer matchId = BusinessQuery.newMatch(game);
-		Integer turnId = BusinessQuery.newTurn(game.getActualPlayer(), matchId, false);
-
-		ArrayList<Card> topCard = game.getStack().getStack();
-
-		BusinessQuery.addCardToStack(topCard.get(0), matchId, turnId);
-
-		for (int i = 0; i < arrayPlayer.size(); i++) {
-			for (int j = 0; j < arrayPlayer.get(i).getCards().size(); j++) {
-				BusinessQuery.addCardToPlayerHand(arrayPlayer.get(i),
-						arrayPlayer.get(i).getCards().get(j), matchId, turnId);
-			}
+		// Add stack
+		Card topCard = game.getStack().getStack().get(0);
+		StackObject stack = new StackObject();
+		stack.setIdCard(((CardDAO)DAOFactory.getCardDAO()).find(topCard.getColor().getNumber(), topCard.getValue().getNumber()).getId());
+		stack.setIdMatch(match.getId());
+		stack.setIdTurn(turn.getId());
+		DAOFactory.getStackDAO().create(stack);
+		
+		// Add hand
+		for (int i = 0; i < players.size(); i++) {
+			HandPlayerObject hand = new HandPlayerObject();
+			hand.setIdMatch(match.getId());
+			hand.setIdTurn(turn.getId());
+			hand.setIdUser(players.get(i).getIdUser());
+			hand.setCard(game.getPlayers().get(i).getCards());
+			DAOFactory.getHandPlayerDAO().create(hand);
+			
 		}
 	}
 
 	private void saveTurn(Game game) throws Exception {
-		/*
-		 * Game id a partir du gameName
-		 */
-		String gameName = game.getGameName();
-		int gameId = gameDAO.getIdgameWithName(gameName);
+	
+			GameObject gameObject = ((GameDAO)DAOFactory.getGameDAO()).find(game.getGameName());
+	
+			int gameId= gameObject.getId();
+
+
+			/*
+			 
+		     * MatchId a partir du GameId
+		 
+		     */
+			
+			MatchObject matchObject = ((MatchDAO)DAOFactory.getMatchDAO()).findbyGameId(gameId);
+			
+		    int matchId = matchObject.getId();
+		        
+		 
+			TurnObject turn = new TurnObject();
+			turn.setIdMatch(matchId);
+			UserObject user = ((UserDAO)DAOFactory.getUserDAO()).find(game.getActualPlayer().getName());
+			turn.setIdUser(user.getId());
+			turn.setInverded(game.getDirection());
+			DAOFactory.getTurnDAO().create(turn);
+		    
+			
+
+		 
+			Card topCard = game.getStack().getStack().get(0);
+		 
+			StackObject stack = new StackObject();
+			stack.setIdCard(((CardDAO)DAOFactory.getCardDAO()).find(topCard.getColor().getNumber(), topCard.getValue().getNumber()).getId());
+			stack.setIdMatch(matchId);
+			stack.setIdTurn(turn.getId());
+			DAOFactory.getStackDAO().create(stack);
+			 
+		    
+
+		 
+		    List<Player> arrayPlayer = game.getPlayers();
+		 
+		    for (int i = 0; i < arrayPlayer.size(); i++) {
+		 
+				UserObject userPlayer = ((UserDAO)DAOFactory.getUserDAO()).find(arrayPlayer.get(i).getName());
+		    	
+				HandPlayerObject hand = new HandPlayerObject();
+				hand.setIdMatch(matchId);
+				hand.setIdTurn(turn.getId());
+				hand.setIdUser(userPlayer.getId());
+				hand.setCard(arrayPlayer.get(i).getCards());
+				DAOFactory.getHandPlayerDAO().create(hand);
+		   
+		      }
+		 
+		    }
+		 
 		
-		/*
-		 * MatchId a partir du GameId
-		 */
-		int matchId = gameDAO.getIdMatchWithGameId(gameId);
-		boolean inversed = game.getDirection();
-
-		int turnId = BusinessQuery.newTurn(game.getActualPlayer(),
-				matchId, inversed);
-
-		ArrayList<Card> topCard = game.getStack().getStack();
-		BusinessQuery.addCardToStack(topCard.get(0), matchId, turnId);
-
-		List<Player> arrayPlayer = game.getPlayers();
-
-		for (int i = 0; i < arrayPlayer.size(); i++) {
-			for (int j = 0; j < arrayPlayer.get(i).getCards().size(); j++) {
-				BusinessQuery.addCardToPlayerHand(arrayPlayer.get(i),
-						arrayPlayer.get(i).getCards().get(j), matchId, turnId);
-			}
-		}
+		
 	}
 
-}
