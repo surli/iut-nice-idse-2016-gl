e8a5e8d240fc14755f9f85e327cee747e8b28b04
Régis Parpex <regis.parpex@gmail.com>
2016-04-24 12:48:31 +0200

    LPSUA-343 - [Moteur][IA] Remplacement des variables Global, par des variables protected avec leur Getter/Setter pour l'IAHard LPSUA-344 - [Moteur][IA] Refactorisation class IAHard pour correspondre au ticket LPSUA-338

--- src/main/java/fr/unice/idse/model/player/IAHard.java
+++ src/main/java/fr/unice/idse/model/player/IAHard.java
@@ -7,46 +7,103 @@ import fr.unice.idse.model.card.NumberCardByColor;
 import fr.unice.idse.model.card.Value;
 
 import java.util.ArrayList;
-import java.util.Collections;
 
 public class IAHard extends IA {
 
-    //CONSTANTE
-    private static Card myCard;
-    private static boolean turnPlay = false;
+// ------------------------------------------------------------------- myCard
+    /** La carte qui sera joué. */
+    protected Card myCard;
 
-
-    //GETTER
+    /**
+     * Getter de la carte qui sera joué.
+     * @return Card La carte qui sera joué.
+     */
     public Card getMyCard() {
-        return myCard;
+        return this.myCard;
     }
 
-    public boolean getTurnPlay () {
-        return turnPlay;
-    }
-
-
-    //SETTER
+    /**
+     * Setter de la carte qui sera joué.
+     * @param myCard La nouvelle carte qui sera joué.
+     */
     public void setMyCard(Card myCard) {
         this.myCard = myCard;
     }
 
+// ------------------------------------------------------------------- turnPlay
+    /** Détermine si l'IA est prête a joué son tour. */
+    protected boolean turnPlay;
+
+    /**
+     * Getter l'IA est prête a joué.
+     * @return boolean l'IA est prête a joué.
+     */
+    public boolean getTurnPlay() {
+        return this.turnPlay;
+    }
+
+    /**
+     * Setter l'IA est prête a joué.
+     * @param turnPlay Nouveau statut l'IA est prête a joué
+     */
+    public void setTurnPlay(boolean turnPlay) {
+        this.turnPlay = turnPlay;
+    }
+
+// ------------------------------------------------------------------- bestColor
+    /** Détermine la meilleur couleur en main. */
+    protected Color bestColor;
+
+    /**
+     * Getter de la meilleur couleur en main.
+     * @return Color la meilleur couleur en main.
+     */
+    public Color getBestColor() {
+        return this.bestColor;
+    }
+
+    /**
+     * Setter la meilleur couleur en main.
+     * @param bestColor Détermine la nouvelle la meilleur couleur en main.
+     */
+    public void setBestColor(Color bestColor) {
+        this.bestColor = bestColor;
+    }
+
+
+// ------------------------------------------------------------------- cardContre
+
+    /** Détermine la meilleur carte pour contré le joueur suivant. */
+    protected Card cardContre;
+
+    /**
+     * Getter de la meilleur carte pour contré le joueur suivant
+     * @return Card la meilleur carte pour contré le joueur suivant
+     */
+    public Card getCardContre() {
+        return this.cardContre;
+    }
+
+    /**
+     * Setter de la meilleur carte pour contré le joueur suivant
+     * @param cardContre Détermine la nouvelle meilleur carte pour contré le joueur suivant.
+     */
+    public void setCardContre(Card cardContre) {
+        this.cardContre = cardContre;
+    }
 
     //CONSTRUCTEUR
     public IAHard(String name, String token, int difficulty) {
         super(name, token, difficulty);
     }
 
-    static Color bestColor = null;
-    static Card cardContre;
 
     public void thinking (Game game) {
         ArrayList<Card> mainIA = game.getActualPlayer().getCards();
         ArrayList<Card> playableCards = game.playableCards();
-        System.out.println("Carte jouable : " + playableCards.toString());
 
-        myCard = chooseCardToPlay(mainIA, playableCards, game);
-        playCard(game, myCard, mainIA, turnPlay);
+        setMyCard(chooseCardToPlay(mainIA, playableCards, game));
+        playCard(game, getMyCard(), mainIA, getTurnPlay());
     }
 
     public Card chooseCardToPlay (ArrayList<Card> mainIA, ArrayList<Card> playableCards, Game game) {
@@ -57,26 +114,26 @@ public class IAHard extends IA {
         Card myCard = null;
         int i = 0;
 
-        turnPlay = checkNextPlayer(game);
+        setTurnPlay(checkNextPlayer(game));
 
-        while (!turnPlay && nbCard < cards.get(3).getNumber()) {
+        while (!getTurnPlay() && nbCard < cards.get(3).getNumber()) {
             myCard = mainIA.get(i);
-            turnPlay = testCardPlayable(playableCards, myCard);
+            setTurnPlay(testCardPlayable(playableCards, myCard));
             nbCard++;
         }
-        while (!turnPlay && nbCard < cards.get(2).getNumber()) {
+        while (!getTurnPlay() && nbCard < cards.get(2).getNumber()) {
             myCard = mainIA.get(i);
-            turnPlay = testCardPlayable(playableCards, myCard);
+            setTurnPlay(testCardPlayable(playableCards, myCard));
             nbCard++;
         }
-        while (!turnPlay && nbCard < cards.get(1).getNumber()) {
+        while (!getTurnPlay() && nbCard < cards.get(1).getNumber()) {
             myCard = mainIA.get(i);
-            turnPlay = testCardPlayable(playableCards, myCard);
+            setTurnPlay(testCardPlayable(playableCards, myCard));
             nbCard++;
         }
-        while (!turnPlay && nbCard < cards.get(0).getNumber()) {
+        while (!getTurnPlay() && nbCard < cards.get(0).getNumber()) {
             myCard = mainIA.get(i);
-            turnPlay = testCardPlayable(playableCards, myCard);
+            setTurnPlay(testCardPlayable(playableCards, myCard));
             nbCard++;
         }
 
@@ -87,16 +144,17 @@ public class IAHard extends IA {
         System.out.println("Appel de la fonction testCardPlayableAndPlay ");
         for (Card aCard : playableCards) {
             if (myCard == aCard) {
-                turnPlay = true;
+                setTurnPlay(true);
                 break;
             }
         }
-        return turnPlay;
+        return getTurnPlay();
     }
 
 
     public boolean checkNextPlayer (Game game) {
         int nextPlayer = game.getNextPlayer();
+
         ArrayList<Card> mainIA = game.getActualPlayer().getCards();
 
         int nbCardMainNextPlayer = game.getPlayers().get(nextPlayer).getCards().size();
@@ -104,31 +162,32 @@ public class IAHard extends IA {
         if(nbCardMainNextPlayer == 1) {
 
             if(searchColorCard(mainIA, Color.Black)) {   // ESSAYER DE JOUER CHANGE COLOUR OU +4
-                turnPlay = true;
+                setTurnPlay(true);
             }/* Bug - Il faut verifie la valeur + la couleur pour jouer cette carte
             else if (searchValueCard(mainIA, Value.DrawTwo)) {
             	game.poseCard(cardContre);
                 turnPlay = true;
             }*/
             else if (searchValueCard(mainIA, game.getStack().topCard().getValue())) {  //MEME NOMBRE QUE LA DERNIERE CARTE POSE
-                turnPlay = true;
+                setTurnPlay(true);
             }
         }
 
-        return turnPlay;
+        return getTurnPlay();
     }
 
 
     /**
      * Retourne true si le joueur a des cartes de couleur precise dans sa main en paramettre sinon retourne false
      */
+    @Override
     public boolean searchColorCard (ArrayList<Card> main, Color colorCard) {
         boolean colorExist = false;
 
         for (Card aCard : main) {
             if(aCard.getColor() == colorCard) {
                 colorExist = true;
-                cardContre = aCard;
+                setCardContre(aCard);
             }
         }
 
@@ -138,37 +197,26 @@ public class IAHard extends IA {
     /**
      * Retourne true si le joueur a des cartes de valeur precise dans sa main en paramettre sinon retourne false
      */
+    @Override
     public boolean searchValueCard (ArrayList<Card> main, Value valueCard) {
         boolean valueExist = false;
         for (Card aCard : main) {
             if(aCard.getValue() == valueCard) {
                 valueExist = true;
-                cardContre = aCard;
+                setCardContre(aCard);
             }
         }
 
         return valueExist;
     }
 
-    public Color chooseColor() {
-        return bestColor;
+    @Override
+    public Color chooseColor(ArrayList<Card> mainIA) {
+        return getBestColor();
     }
 
-    public void changeColor(ArrayList<Card> mainIA, Game game) {
-        game.getAlternative().getEffectCard(myCard).action(chooseColor());
-    }
-
-    public void playCard (Game game, Card cardToPlay, ArrayList<Card> mainIA, boolean turnPlay) {
-        if(turnPlay) {
-            game.poseCard(cardToPlay);
-            System.out.println("Carte joué : " + cardToPlay);
-
-            if (game.getAlternative().getEffectCard(cardToPlay).isColorChangingCard()) {
-                changeColor(mainIA, game);
-            }
-        }
-        else {
-            game.drawCard();
-        }
+    @Override
+    public void changeColor(Card cardToPlay, ArrayList<Card> mainIA, Game game) {
+        game.getAlternative().getEffectCard(getMyCard()).action(chooseColor(mainIA));
     }
 }
