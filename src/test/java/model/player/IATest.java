65cc8ef5907dedb848e2db0d532a6eae908e1597
Régis Parpex <regis.parpex@gmail.com>
2016-04-24 12:58:10 +0200

    LPSUA-345 - [Moteur][IA] Test de la méthode testSearchColorCard LPSUA-346 - [Moteur][IA] Test de la méthode testSearchValueCard

--- src/test/java/model/player/IATest.java
+++ src/test/java/model/player/IATest.java
@@ -1,5 +1,67 @@
 package model.player;
 
+import fr.unice.idse.model.card.Card;
+import fr.unice.idse.model.card.Color;
+import fr.unice.idse.model.card.Value;
+import fr.unice.idse.model.player.IA;
+import fr.unice.idse.model.player.IAFactory;
+import org.junit.Before;
+import org.junit.Test;
+
+import java.util.ArrayList;
+
+import static org.junit.Assert.assertEquals;
+
 public class IATest {
 
+    private IA iaHard;
+
+    @Before
+    public void initialize(){
+
+        iaHard = IAFactory.getIA("testIAHard", "", 3);
+
+        ArrayList<Card> cards=new ArrayList<Card>();
+
+        cards.add(new Card(Value.Three, Color.Blue));
+        cards.add(new Card(Value.Six, Color.Green));
+
+        iaHard.setCards(cards);
+
+    }
+
+    /* ------------ testSearchColorCard ----------------------------- */
+    @Test
+    public void testSearchColorCardTrue() {
+
+        boolean expected = true;
+        boolean colorExist = iaHard.searchColorCard(iaHard.getCards(), Color.Green);
+        assertEquals(expected, colorExist);
+    }
+
+    @Test
+    public void testSearchColorCardFalse() {
+
+        boolean expected = false;
+        boolean colorExist = iaHard.searchColorCard(iaHard.getCards(), Color.Red);
+        assertEquals(expected, colorExist);
+    }
+
+/* ------------ testSearchValueCard ----------------------------- */
+
+    @Test
+    public void testSearchValueCardTrue() {
+
+        boolean expected = true;
+        boolean valueExist = iaHard.searchValueCard(iaHard.getCards(), Value.Six);
+        assertEquals(expected, valueExist);
+    }
+
+    @Test
+    public void testSearchValueCardFalse() {
+
+        boolean expected = false;
+        boolean valueExist = iaHard.searchValueCard(iaHard.getCards(), Value.Nine);
+        assertEquals(expected, valueExist);
+    }
 }
