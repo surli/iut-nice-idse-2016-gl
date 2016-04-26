2b5179cdfafd0e8dede9687c1dd872cdf6cd6221
Tom Phily <tom.phily@live.fr>
2016-04-26 15:27:40 +0200

    LPSUA-365 : Ajout dun HashMap dans conf.java pour repertorier les variantes

--- src/main/java/fr/unice/idse/constante/Config.java
+++ src/main/java/fr/unice/idse/constante/Config.java
@@ -1,5 +1,12 @@
 package fr.unice.idse.constante;
 
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Map;
+
+import fr.unice.idse.model.regle.*;
+import fr.unice.idse.model.card.Value;;
+
 /**
  * Classe config
  */
@@ -9,4 +16,18 @@ public final class Config {
 	public static String url = "jdbc:mysql://localhost:" + port + "/uno";
 	public static String user = "uno";
 	public static String pass = "uno";
+	public static Map<String, ArrayList<EffectCard>> variantes = new HashMap<String, ArrayList<EffectCard>>()
+			{{
+				put("variante1", new ArrayList<EffectCard>()
+				{{
+					new RuleChangeColor(Value.Wild);
+					new RuleDrawFour(Value.DrawFour);
+					new RuleDrawTwo(Value.DrawTwo);
+					new RuleReverse(Value.Reverse);
+					new RuleRotatePlayerDecks(Value.Zero);
+					new RuleSkip(Value.Skip);
+					new RuleTradePlayerDecks(Value.Seven);
+				}});
+				//Todo : Ecrire les prochaines variantes ici
+			}};
 }
